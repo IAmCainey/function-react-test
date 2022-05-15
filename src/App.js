0058e79c9a2b38cfb18541cb41a18a63fff359a5
Paul Caine <paulcaine@tutanota.com>
2022-05-15 23:23:03 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function App(props) {
+  const name = "paul caine";
+  const age = "40, only just";
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>functions and testing</h1>
+      <p>this is all just a test</p>
+      <h2>click counter</h2>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>add 1</button>
+      <button onClick={() => setCount(count - 1)}>minus 1</button>
+      <br />
+      <p>
+        name : <span>{name}</span> <br />
+        fooking age : <span>{age}</span>
+      </p>
+
+      <p>test the button above</p>
+      <p>
+        onso did you know that the number above times by 2 is the number below!
+      </p>
+      <h1>{count * 2}</h1>
+      <p>
+        this is a cool way to learn react{" "}
+        <b>
+          <i>useStats</i>
+        </b>
+      </p>
+
+      <section>
+        <h2>look at me dad, im coding!</h2>
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.giphy.com%2Fmedia%2FdZX3AduGrY3uJ7qCsx%2Fgiphy.gif&f=1&nofb=1"
+          alt="a fucking gif!"
+        />
+      </section>
+
+      <section>
+        <h1>i wouldnt!!!!</h1>
+
+        <button onClick={() => alert("i fucking told you")}>
+          {" "}
+          dont do it!
+        </button>
+      </section>
     </div>
   );
 }
